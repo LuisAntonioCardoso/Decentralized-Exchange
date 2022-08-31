268f8fbe768f667c7ce6885d0bd8541d05b0acc1
LuisAntonioCardoso <luis.antonio.c.of@gmail.com>
2022-08-31 20:04:06 +0100

    5. Token Transfers

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -15,15 +15,40 @@ contract Token {
     uint256 public totalSupply; // we have 1000 total supply, but we need to consider the decimals
 
     mapping(address => uint256) public balanceOf; // save balances of the addresses
-    
 
-    constructor(string memory _name,
-                string memory _symbol,
-                uint256 _totalSupply) {
+    event Transfer( 
+        address indexed from,
+        address indexed to,
+        uint256 value); // event to be called when a transfer is done
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply) 
+    {
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply * (10**decimals);
         balanceOf[msg.sender] = totalSupply; // atribute the total supply of the curency will be sent to the creator
+    }
+
+    function transfer(
+            address _to, 
+            uint256 _value) 
+        public returns(
+            bool sucess) 
+    {
+        // verify if sender has enough tokens to send
+        require(balanceOf[msg.sender] >= _value);
+        // check if sender is not the receiver
+        require(_to != address(0));
+        // debit sender
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+        // credit receiver
+        balanceOf[_to] = balanceOf[_to] + _value;
+        // emit event
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 
 }
--- test/Token.js
+++ test/Token.js
@@ -11,7 +11,8 @@ describe('Token', () => {
   // we put the variable here so that it can be used by all the tests
   let token, 
       accounts,
-      deployer;
+      deployer,
+      receiver;
 
   // we need to indicate that we are using async functions 
   beforeEach( async() => {
@@ -22,9 +23,10 @@ describe('Token', () => {
     // get accounts
     accounts = await ethers.getSigners();
     deployer = accounts[0];
-  })
+    receiver = accounts[1];
+  });
 
-  describe('deployment', () => {
+  describe('Deployment', () => {
     const name = 'My Token';
     const symbol = 'MTK';
     const decimals = 18;
@@ -49,6 +51,52 @@ describe('Token', () => {
     it('assigns total supply to deployer', async () => { 
       expect(await token.balanceOf(deployer.address)).to.equal(totalSupply);
     })
-  })
+  });
 
+  describe('Sending Token', async () => {
+
+    let amount,
+        transaction,
+        result;
+    
+    describe('success', () => {
+
+      beforeEach( async () => {
+        amount = tokenToDecimal('100');
+        transaction = await token.connect(deployer).transfer(receiver.address, amount); // connect sender to sign the transaction and do the transaction
+        result = await transaction.wait();  // wait for the transaction to get included in a block
+      });
+  
+      it('accounts have correct balances', async () => {
+  
+        expect(await token.balanceOf(deployer.address)).to.equal(tokenToDecimal('900'));
+        expect(await token.balanceOf(receiver.address)).to.equal(amount);
+  
+      });
+  
+      it('emits a transfer event', async () => {
+        const event = result.events[0]; 
+        expect(event.event).to.equal('Transfer');
+        expect(event.args.from).to.equal(deployer.address);
+        expect(event.args.to).to.equal(receiver.address);
+        expect(event.args.value).to.equal(amount);
+      });
+    });
+
+    describe('failure', () => {
+
+      it('rejects insufficient balance', async () => {
+  
+        const invalidAmount = tokenToDecimal('10000');
+        await expect(token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted;
+      });
+
+      it('rejects invalid recipient', async () => {
+  
+        const amount = tokenToDecimal('100');
+        await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted;
+      });
+    });
+
+  });
 });
