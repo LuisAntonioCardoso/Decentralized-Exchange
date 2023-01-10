7f99169a4ce0f566967eacc4b94ba8c5ba95d553
LuisAntonioCardoso <luis.antonio.c.of@gmail.com>
2023-01-10 22:27:19 +0000

    change to App.js by applying prettier format

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,13 @@ import { useDispatch } from 'react-redux';
 import config from '../config.json';
 
 import {
-  loadProvider,
-  loadNetwork,
-  loadAccount,
-  loadTokens,
-  loadExchange,
-  loadAllOrders,
-  subscribeToEvents
+	loadProvider,
+	loadNetwork,
+	loadAccount,
+	loadTokens,
+	loadExchange,
+	loadAllOrders,
+	subscribeToEvents
 } from '../store/interactions.js';
 
 import Navbar from './Navbar';
@@ -22,87 +22,79 @@ import Trades from './Trades';
 import OrderBook from './OrderBook';
 
 function App() {
+	const dispatch = useDispatch();
 
-  const dispatch = useDispatch();
+	/**
+	 * function where we gather information about the blockchain
+	 * this includes:
+	 *    - the node we are using to interact with the blockchain (provider)
+	 *    - the blockchain ID
+	 *    - connect the browser wallet
+	 *    - the smart contract addresses
+	 */
+	const loadBlockchainData = async () => {
+		// connect Ethers to blockchain
+		const provider = loadProvider(dispatch); // provider is going to be our connection to the blockchain
 
-  /**
-   * function where we gather information about the blockchain
-   * this includes:
-   *    - the node we are using to interact with the blockchain (provider)
-   *    - the blockchain ID
-   *    - connect the browser wallet
-   *    - the smart contract addresses
-   */
-  const loadBlockchainData = async () => {
+		// fetch current network's chainId (hardhat:31337, kovan:42)
+		const chainId = await loadNetwork(dispatch, provider);
 
-    // connect Ethers to blockchain
-    const provider = loadProvider(dispatch); // provider is going to be our connection to the blockchain
+		// Reload page when network changes
+		window.ethereum.on('chainChanged', () => {
+			window.location.reload();
+		});
 
-    // fetch current network's chainId (hardhat:31337, kovan:42)
-    const chainId = await loadNetwork(dispatch, provider);
+		// if the account in metamask change, then load new account
+		window.ethereum.on('accountsChanged', () => {
+			// TODO: we should check if it was previously loaded
+			// Fetch current account and balance from metamask
+			loadAccount(dispatch, provider);
+		});
 
-    // Reload page when network changes
-    window.ethereum.on('chainChanged', () => {
-      window.location.reload();
-    });
+		// Load tokens and exchange smart contracts
+		const mDAI = config[chainId].mDAI;
+		const mETH = config[chainId].mETH;
+		await loadTokens(dispatch, provider, [mDAI.address, mETH.address]);
 
-    // if the account in metamask change, then load new account
-    window.ethereum.on('accountsChanged', () => {
-      // TODO: we should check if it was previously loaded
-      // Fetch current account and balance from metamask
-      loadAccount(dispatch, provider);
-    });
+		const exchangeConfig = config[chainId].exchange;
+		const exchange = await loadExchange(dispatch, provider, exchangeConfig.address);
 
-    // Load tokens and exchange smart contracts
-    const mDAI = config[chainId].mDAI;
-    const mETH = config[chainId].mETH;
-    await loadTokens(dispatch, provider, [mDAI.address, mETH.address]);
+		loadAllOrders(dispatch, provider, exchange);
 
-    const exchangeConfig = config[chainId].exchange;
-    const exchange = await loadExchange(dispatch, provider, exchangeConfig.address);
+		subscribeToEvents(dispatch, exchange);
+	};
 
-    loadAllOrders(dispatch, provider, exchange);
+	// useEffect is what is going to run once the app component is loaded
+	useEffect(() => {
+		loadBlockchainData();
+	});
 
-    subscribeToEvents(dispatch, exchange);
-  }
+	return (
+		<div>
+			<Navbar />
 
-  // useEffect is what is going to run once the app component is loaded
-  useEffect(() => {
-    loadBlockchainData();
-  })
+			<main className='exchange grid'>
+				<section className='exchange__section--left grid'>
+					<Markets />
 
-  return (
-    <div>
+					<Balance />
 
-      <Navbar/>
+					<Order />
+				</section>
+				<section className='exchange__section--right grid'>
+					<PriceChart />
 
-      <main className='exchange grid'>
-        <section className='exchange__section--left grid'>
+					<Transactions />
 
-          <Markets/>
+					<Trades />
 
-          <Balance/>
+					<OrderBook />
+				</section>
+			</main>
 
-          <Order/>
-
-        </section>
-        <section className='exchange__section--right grid'>
-
-          <PriceChart/>
-
-          <Transactions/>
-
-          <Trades/>
-
-          <OrderBook/>
-
-        </section>
-      </main>
-
-      {/* Alert */}
-
-    </div>
-  );
+			{/* Alert */}
+		</div>
+	);
 }
 
 export default App;
